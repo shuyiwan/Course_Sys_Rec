c267017941d0f3c49b3fb353ef6b46bb9748b73c
Shuyi Wan <shuyiwan@ucsb.edu>
2024-01-26 19:54:44 -0800

    created course cart page

--- MVP/frontend/src/App.js
+++ MVP/frontend/src/App.js
@@ -3,6 +3,7 @@ import './index.css';
 import Navbar from './Components/Navbar'
 import About from './Pages/About'
 import Courses from './Pages/Courses'
+import CourseCart from './Pages/CourseCart';
 import Home from './Pages/Home'
 import {Route, Routes} from 'react-router-dom'
 
@@ -19,6 +20,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Home/>}/>
             <Route path="/courses" element={<Courses/>}/>
+            <Route path="/coursecart" element={<CourseCart/>}/>
             <Route path="/about" element={<About/>}/>
           </Routes>
         </div>
---
+++ MVP/frontend/src/Pages/CourseCart.js
@@ -0,0 +1,3 @@
+export default function CourseCart(){
+    return <h1>CourseCart Page Not Implemented yet</h1>
+}
