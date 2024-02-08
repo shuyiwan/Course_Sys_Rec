2755eb0c6af6688c2ebd69a274b7f3955a85a9c2
HououinKyouma-2036 <lixing_guo@ucsb.edu>
2024-02-07 23:51:39 -0800

    Implement About Page

--- MVP/frontend/src/Components/SearchPageResult.js
+++ MVP/frontend/src/Components/SearchPageResult.js
@@ -1,16 +1,17 @@
 import React from "react";
 import '../Styles/SearchPageResult.css';
 
-export default function SearchPageResult({ result, addToCart }) {
-    const handleAddToCart = (e) => {
-        e.stopPropagation();
-        addToCart(result);
-    };
-
+export default function SearchPageResult({ result }) {
     return (
-        <div className="SearchPageResult" onClick={(e) => alert('Click on ' + result.title)}>
-            {result.title}
-            <button onClick={handleAddToCart} className="AddToCartButton">+</button>
+        <div className="SearchPageResult">
+            <p>{result.courseID}</p>
+            <br />
+            <p>{result.title}</p>
+            <br />
+            <p>Description: {result.description}</p>
+            <br />
+            <p>Instructor: {result.instructor}</p>
+            <button className="AddToCartButton">+</button>
         </div>
     );
 }
