adcc5762fd26214fc5c930c47d801902d152f2bb
Yicong Yan <yicongyan@ucsb.edu>
2024-02-07 22:08:37 -0800

    Add backend API and use it to get the courses on the search page

--- MVP/frontend/src/Components/SearchResult.js
+++ MVP/frontend/src/Components/SearchResult.js
@@ -1,8 +1,13 @@
 import React from "react"
 import '../Styles/SearchResult.css'
+import {Link} from "react-router-dom"
 
 export default function SearchResult({result}){
     return (
-        <div className="searchResult" onClick={(e) => alert('Click on ' + result.name)}>{result.name}</div>
+        <div className="searchResult">
+            {/* <Link to="/search" style={{ color: 'black'}}> */}
+                {result.courseID}
+            {/* </Link> */}
+        </div>
     )
 }
