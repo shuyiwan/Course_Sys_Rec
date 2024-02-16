e6d6a9237e8045bcd21152bce155a4aae8f59369
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2024-02-16 12:54:54 -0800

    add extra test

--- backend/search/tests.py
+++ backend/search/tests.py
@@ -73,7 +73,7 @@ class StoreCoursesTest(TestCase):
             call(f"Skipping: {query['classes'][1]['courseId']}"),
         ]
 
-    def test_store_department_false(self):
+    def test_store_department_empty(self):
         year = 2024
         quarter = 1
         code = "CMPSC"
@@ -81,7 +81,7 @@ class StoreCoursesTest(TestCase):
         self.assertFalse(ucsb_api.already_stored_department(year, quarter, code, total_courses))
     
     @patch('builtins.print')
-    def test_store_department_true(self, mock_print):
+    def test_store_department_full(self, mock_print):
         year = 2024
         quarter = 1
         code = "CMPSC"
@@ -103,3 +103,29 @@ class StoreCoursesTest(TestCase):
         ucsb_api.store_courses(query, year, quarter, code)
 
         self.assertTrue(ucsb_api.already_stored_department(year, quarter, code, total_courses))
+
+    @patch('builtins.print')
+    def test_store_department_partially_full(self, mock_print):
+        year = 2024
+        quarter = 1
+        code = "CMPSC"
+        stored_courses = 15
+        total_courses = 30
+        UCSB_quarter = f"{year}{quarter}"
+        query = {
+            "pageNumber": 1,
+            "pageSize": 30,
+            "total": total_courses,
+            "classes": []
+        }
+
+        for i in range(stored_courses):
+            query["classes"].append({
+                "quarter": int(UCSB_quarter),
+                "courseId": f"CMPSC {i}",
+                "description": "TMP",
+            })
+        ucsb_api.store_courses(query, year, quarter, code)
+
+        self.assertFalse(ucsb_api.already_stored_department(year, quarter, code, total_courses))
+
