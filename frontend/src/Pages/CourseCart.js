4095a5fc8e0ff6acd9bd00014b9ebcc62d4dc76e
Yicong Yan <yicongyan@ucsb.edu>
2024-03-01 22:46:55 -0800

    update the url in the frontend

--- frontend/src/Pages/CourseCart.js
+++ frontend/src/Pages/CourseCart.js
@@ -8,11 +8,11 @@ export default function CourseCart() {
     const [cartItems, setCartItems] = useState([]);
     
     async function getCourseList() {
-        const response = await fetch(`http://127.0.0.1:8000/shoppingCart/retrieve/?email=`+localStorage.getItem("email"), {
+        const response = await fetch(`https://intermittence.pythonanywhere.com/shoppingCart/retrieve/?email=`+localStorage.getItem("email"), {
             credentials: 'include',
         });
         const data = await response.json();
-        console.log(data)
+        //console.log(data)
         setCartItems(data); 
     }
 
@@ -27,34 +27,46 @@ export default function CourseCart() {
         updatedCart.splice(index, 1);
         setCartItems(updatedCart);
         let ID = cartItems[index].courseID
+        let sqlID = cartItems[index].sql_id
         // ID = ID.replace(/\s/g, '')
         console.log(ID)
         //http://127.0.0.1:8000/shoppingCart/delete/?email=test@ucsb.edu&courseID=CS148
-        const response = await fetch('http://127.0.0.1:8000/shoppingCart/delete/?email='
-        +localStorage.getItem("email") + '&courseID=' + ID, {
+        const response = await fetch('https://intermittence.pythonanywhere.com/shoppingCart/delete/?email='
+        +localStorage.getItem("email") + '&courseID=' + ID + '&sql_id=' + sqlID, {
             method: 'GET',
         });
         const data = await response.json();
-        console.log(data)
+        //console.log(data)
         //getCourseList();
     }
 
-    return (
-        <div>
-
-            <div id="shopping-cart">
-                {cartItems.map((course, id) => (
-                    <div className="cart-item" key={id}>
-                        <button className="remove-item" onClick={() => removeItem(id)}>Remove</button>
-                        <h2>{course.courseID}</h2>
-                        <p>{course.description}</p>
-                    </div>
-                ))}
+    if (cartItems.hasOwnProperty("Empty")){
+        return(
+            // need someone to work on the css for this message
+            <div>
+                Nothing in the cart....
             </div>
-
-            <Link to="/search">Back to Search</Link>
-
-            <button id="checkout-button">Download PDF</button>
-        </div>
-    );
+        )
+    }
+        
+    else{
+        return (
+            <div>
+    
+                <div id="shopping-cart">
+                    {cartItems.map((course, id) => (
+                        <div className="cart-item" key={id}>
+                            <button className="remove-item" onClick={() => removeItem(id)}>Remove</button>
+                            <h2>{course.courseID}</h2>
+                            <p>{course.description}</p>
+                        </div>
+                    ))}
+                </div>
+    
+                <Link to="/search">Back to Search</Link>
+    
+                <button id="checkout-button">Download PDF</button>
+            </div>
+        );
+    }  
 }
