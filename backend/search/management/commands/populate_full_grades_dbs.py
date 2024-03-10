30a3074017e0bd52e6cefeb30514ea3e98084e04
Kevin Nguyen <kevinhnguyen@ucsb.edu>
2024-03-09 19:46:13 -0800

    minor changes + add csv

--- backend/search/management/commands/populate_full_grades_dbs.py
+++ backend/search/management/commands/populate_full_grades_dbs.py
@@ -6,7 +6,8 @@ class Command(BaseCommand):
     help = 'Populate full_grades model from CSV'
 
     def handle(self, *args, **options):
-        with open('~/Downloads/full_grades.csv', 'r') as file:
+        i = 0
+        with open('full_grades.csv', 'r') as file:
             reader = csv.reader(file)
             next(reader)  # Skip the header row
             for row in reader:
@@ -14,9 +15,12 @@ class Command(BaseCommand):
                     # Map CSV fields to model fields
                     quarter=row[0],
                     course_level=row[1],
-                    course=row[2],
+                    course=row[2].replace(" ", ""),
                     instructor=row[3],
                     grade=row[4],
                     student_count=int(row[5])
                 )
+                if i % 1000 == 0:
+                    print(f"Processed {i} rows")
+                i += 1
         self.stdout.write(self.style.SUCCESS('Successfully populated full_grades from CSV'))
