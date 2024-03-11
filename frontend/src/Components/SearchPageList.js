4e5ab7f5bc510df0a0487175c2708669d66cdc8b
Yicong Yan <yicongyan@ucsb.edu>
2024-03-10 18:18:31 -0700

    fix css in filter

--- frontend/src/Components/SearchPageList.js
+++ frontend/src/Components/SearchPageList.js
@@ -7,18 +7,21 @@ export default function SearchPageList({results}){
     //console.log(results)
     const [subjectCodes, setSubjectCodes] = useState([...new Set(results.map((courses) => courses.subject_code))]);
     const [searchResult, setSearchResult] = useState(results);
-    useEffect(()=> {
-      // console.log(searchResult)
-      // console.log(subjectCodes)
-    },[])
+    
     return (
+        
+      <div>
         <div className="SearchPageList">
-        <Filter subjectCodes = {subjectCodes} setSubjectCodes = {setSubjectCodes} 
-                courses={searchResult} setCourses={setSearchResult}/>
-
-        {searchResult.map((result, ID) => {
-          return <SearchPageResult result={result} key={ID} />;
-        })}
+          <Filter
+          subjectCodes = {subjectCodes} setSubjectCodes = {setSubjectCodes} 
+          courses={searchResult} setCourses={setSearchResult}
+          />
+          
+          {searchResult.map((result, ID) => {
+            return <SearchPageResult result={result} key={ID}/>;
+          })}
         </div>
+      </div>
+        
     )
 }
