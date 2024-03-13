42809560986af920262459421ce7ba770e36e4d1
HououinKyouma-2036 <lixing_guo@ucsb.edu>
2024-03-13 15:09:09 -0700

    update instructions for homepage

--- frontend/src/Components/ProcessSteps.js
+++ frontend/src/Components/ProcessSteps.js
@@ -12,26 +12,30 @@ const ProcessSteps = () => {
                 <div className="cs-right-section">
                     <div className="cs-item">
                         <span className="cs-number">01</span>
-                        <p className="cs-item-text">
-                            <strong>Get Started</strong> - Type keywords of courses, carrers, or professors in search bar.
+                        <p className="cs-title"> <strong>Get Started</strong></p>
+                            <p className="cs-item-text">
+                            Type keywords of courses, carrers, or professors in search bar.
                         </p>
                     </div>
                     <div className="cs-item">
                         <span className="cs-number">02</span>
+                        <p className="cs-title"> <strong>Discover</strong></p>
                         <p className="cs-item-text">
-                            <strong>Discover</strong> - Get relvant courses across all departments with a single search.
+                            Get relvant courses across all departments with a single search.
                         </p>
                     </div>
                     <div className="cs-item">
                         <span className="cs-number">03</span>
+                        <p className="cs-title"> <strong>DIY Course Cart</strong></p>
                         <p className="cs-item-text">
-                            <strong>DIY Course Cart</strong> - Add courses your personalized cart.
+                            Add courses your personalized cart.
                         </p>
                     </div>
                     <div className="cs-item">
                         <span className="cs-number">04</span>
+                        <p className="cs-title"> <strong>Start Learning Ahead</strong></p>
                         <p className="cs-item-text">
-                            <strong>Start Learning Ahead</strong> - Click on recommended videos and here we go!
+                            Click on recommended videos and here we go!
                         </p>
                     </div>
                 </div>
