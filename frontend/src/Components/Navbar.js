8f782d78346bec01b911fb36f5afbd65362f4da7
Yicong Yan <yicongyan@ucsb.edu>
2024-03-13 16:50:31 -0700

    fix nav bar

--- frontend/src/Components/Navbar.js
+++ frontend/src/Components/Navbar.js
@@ -1,32 +1,50 @@
-import {Link, useMatch, useResolvedPath} from "react-router-dom"
+import { Link, useMatch, useResolvedPath } from "react-router-dom";
 import '../Styles/nav.css';
 import LoginButton from "./LoginButton";
 import logo_image from "../assets/platinumLogoOff.svg";
 
-export default function Navbar(){
-    return <nav className = "nav">
-        <Link to="/" className = "siteTitle">
-            Home 
-        </Link>
-
-        <ul>
-            {/* uncomment the next line only when you need to test the search page */}
-            {/* <CustomLink to = '/search'>Search</CustomLink> */}
-            <CustomLink to = '/coursecart'>CourseCart</CustomLink>
-            <CustomLink to = '/about'>About</CustomLink>
-        </ul>
-        <LoginButton />
-    </nav>
+export default function Navbar() {
+    return (
+        <nav className="nav">
+            <Link to="/" className="siteTitle">
+                <img src={logo_image} alt="logo" style={{ filter: 'invert(100%) brightness(300%) contrast(100%)' }} />
+            </Link>
+            <ul>
+                {/* Uncomment the next line only when you need to test the search page */}
+                {/* <CustomLink to='/search'>Search</CustomLink> */}
+                <CustomLink to='/about'>
+                    <span>About</span>
+                    <div className="flex flex-col items-center"> {/* Flexbox container for the icon and text */}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+                        </svg>
+                        
+                    </div>
+                </CustomLink>
+                <CustomLink to='/coursecart'>
+                    <span>CourseCart</span>
+                    <div className="flex flex-col items-center"> {/* Flexbox container for the icon and text */}
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
+                        </svg>
+                        
+                    </div>
+                </CustomLink>
+            </ul>
+            <LoginButton />
+        </nav>
+    );
 }
 
-function CustomLink({to,children,...props}){
-    const resolvedPath = useResolvedPath(to)
-    const isActive = useMatch({path: resolvedPath.pathname, end: true})
-    return(
-    <li className={(isActive ? "active" : "")}>
-                <Link className="link" to={to} {...props}>
-                    {children}
-                </Link>
-    </li> 
-    )
+function CustomLink({ to, children, ...props }) {
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link className="link" to={to} {...props}>
+                {children}
+            </Link>
+        </li>
+    );
 }
